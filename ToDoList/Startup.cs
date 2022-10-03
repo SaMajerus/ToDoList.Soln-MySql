38898afaa8fe05c959918be1a8d7a97c3fcb9b24
Samuel Majerus <ladolego@gmail.com>
2022-10-03 16:22:26 -0700

    End of code-along work for Lsn 5

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -16,6 +16,13 @@ namespace ToDoList
       Configuration = builder.Build();
     }
 
+  //Configures the Database connection.  (See FMI:  https://www.learnhowtoprogram.com/c-and-net/database-basics/connecting-a-database-to-an-mvc-app-with-mysqlconnector)
+  public static class DBConfiguration   //Configures the Database Connection. 
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+  }
+
+
     public IConfigurationRoot Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
