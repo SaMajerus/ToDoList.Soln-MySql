b00e4b41e78650f338d170d7a8556f1c7f243088
Samuel Majerus <ladolego@gmail.com>
2022-10-04 09:37:49 -0700

    Corrects location of 'DBConfiguration' class.  [NOW the end of code-along work for Lsn 5]

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -16,13 +16,6 @@ namespace ToDoList
       Configuration = builder.Build();
     }
 
-  //Configures the Database connection.  (See FMI:  https://www.learnhowtoprogram.com/c-and-net/database-basics/connecting-a-database-to-an-mvc-app-with-mysqlconnector)
-  public static class DBConfiguration   //Configures the Database Connection. 
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
-  }
-
-
     public IConfigurationRoot Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
@@ -46,5 +39,11 @@ namespace ToDoList
         await context.Response.WriteAsync("Hello World!");
       });
     }
+  }
+
+  //Configures the Database connection.  (See FMI:  https://www.learnhowtoprogram.com/c-and-net/database-basics/connecting-a-database-to-an-mvc-app-with-mysqlconnector)
+  public static class DBConfiguration   //Configures the Database Connection. 
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
   }
 }
