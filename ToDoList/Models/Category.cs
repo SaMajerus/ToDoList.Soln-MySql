fbbd0e7c46ce60ca5ca1a1eb374be99b8dc80313
Samuel Majerus <ladolego@gmail.com>
2022-10-05 10:36:54 -0700

    Uses EF Core to create a 'One-to-Many Relationship' between 'Categories' and 'Items'. (Complete 'Views/Categories/Index.cshtml' code -- which seems to be missing from Lsn 33([question-mark times 2]) -- is courtesy of Skylan).  [End of code-along work for Lsn 33]

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -4,37 +4,13 @@ namespace ToDoList.Models
 {
   public class Category
   {
-    private static List<Category> _instances = new List<Category> {};
+    public Category()
+    {
+      this.Items = new HashSet<Item>();
+    }
+
+    public int CategoryId { get; set; }
     public string Name { get; set; }
-    public int Id { get; }
-    public List<Item> Items { get; set; }
-
-    public Category(string categoryName)
-    {
-      Name = categoryName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Items = new List<Item>{};
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
-    public static List<Category> GetAll()
-    {
-      return _instances;
-    }
-
-    public static Category Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
-
-    public void AddItem(Item item)
-    {
-      Items.Add(item);
-    }
+    public virtual ICollection<Item> Items { get; set; }  //'virtual' keyword "[allows] Entity to use lazy loading to load only the necessary resources from the database" (Lsn 33). 
   }
 }
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,8 +4,14 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)  //Enables ''Lazy-Loading''
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
